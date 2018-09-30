fab79ac5cbc5c68ab2392ac19e5c9c6d13a84f44
aveugene <eugeneavilov@gmail.com>
2018-09-30 21:50:05 +0700

    removed comments

--- Lesson1/Calculator.java
+++ Lesson1/Calculator.java
@@ -1,11 +1,9 @@
-// Реализуйте Калькулятор, который должен уметь выполнять математические операции (+, -, *, /, ^, %) над целыми положительными числами
 class Calculator {
 	public static void main (String[] args) {
 		int firstNumber = 11;
 		int secondNumber = 3;
 		char sign = '%';
 		
-		// check the sign and make result.
 		if (sign == '+') {
 			System.out.println(firstNumber + "" + sign + "" + secondNumber + "=" + (firstNumber + secondNumber));
 		} else if (sign == '-') {
@@ -22,6 +20,7 @@ class Calculator {
 			System.out.println("The operation " + sign + " is invalid.");
 		}
 	}
+	
 	public static int raiseToPower (int base, int exponent){
 		int result = 1;
 		for (int i = 0; i < exponent; i++){
--- Lesson1/Unicode.java
+++ Lesson1/Unicode.java
@@ -1,4 +1,3 @@
-// Выведите в цикле символы кодировки Unicode в диапазоне [9398, 10178]. Если выводятся кракозябры, то [33, 126]
 class Unicode {
 	public static void main (String[] args) {
 		for (int i = 9398; i <= 10178; i++) {
