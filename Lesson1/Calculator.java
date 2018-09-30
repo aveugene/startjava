662b52da68e27482b610636041b20950407b73ea
aveugene <eugeneavilov@gmail.com>
2018-09-30 08:06:55 +0700

    Lesson 1. Code corrections #1

--- Lesson1/Calculator.java
+++ Lesson1/Calculator.java
@@ -1,54 +1,41 @@
-
-
 // Реализуйте Калькулятор, который должен уметь выполнять математические операции (+, -, *, /, ^, %) над целыми положительными числами
-
 class Calculator {
-	public static int exponentiation ( int a, int b ){
+	public static void main (String[] args) {
+		int firstNumber = 11;
+		int secondNumber = 3;
+		char sign = '^';
+		int result = 0;
+		int invalidSign = 0;
+		// check the sign and make result.
+		if (sign == '+') {
+			result = firstNumber + secondNumber;
+		} else if (sign == '-') {
+			result = firstNumber - secondNumber;
+		} else if (sign == '*') {
+			result = firstNumber * secondNumber;
+		} else if (sign == '/') {
+			result = firstNumber / secondNumber;
+		} else if (sign == '^') {
+			result = raiseToPower(firstNumber, secondNumber);
+		} else if (sign == '%') {
+			result = firstNumber % secondNumber;
+		} else {
+			invalidSign = 1;
+		}
+		// print result if sign is ok
+		if (invalidSign == 0) {
+			System.out.println(firstNumber + "" + sign + "" + secondNumber + "=" + result);
+		} else {
+			System.out.println("The operation " + sign + " is invalid.");
+		}
+	}
+	public static int raiseToPower (int base, int exponent){
 		int result = 1;
-		for ( int i=0; i<b; i++ ){
-			result = result * a;
+		for (int i = 0; i < exponent; i++){
+			result = result * base;
 		}
 		return result;
 	}
 
-	public static void main (String[] args) {
-		int a = 11;
-		int b = 3;
-		char sign = '/';
-		int result = 0;
-		int wrongSign = 0;
-
-		// check the sign and make result.
-		if ( sign == '+' ) {
-			result = a + b;
-		}
-		else if ( sign == '-' ) {
-			result = a - b;
-		}
-		else if ( sign == '*' ) {
-			result = a * b;
-		}
-		else if ( sign == '/' ) {
-			result = a / b;
-		}
-		else if ( sign == '^' ) {
-			result = exponentiation( a, b );
-		}
-		else if ( sign == '%' ) {
-			result = a % b;
-		}
-		else {
-			wrongSign = 1;
-		}
-
-		// print result if sign is ok
-		if ( wrongSign == 0 ) {
-			System.out.println( a + "" + sign + "" + b + "=" + result );
-		}
-		else {
-			System.out.println( "Wrong sign entered." );
-		}
-
-	}
 
 }
--- Lesson1/Unicode.java
+++ Lesson1/Unicode.java
@@ -1,17 +1,12 @@
-
 // Выведите в цикле символы кодировки Unicode в диапазоне [9398, 10178]. Если выводятся кракозябры, то [33, 126]
-
 class Unicode {
-	public static void main ( String[] args ) {
-		
-		for ( int i = 9398; i <= 10178; i++ ) {
-			System.out.println ( (char) i );
+	public static void main (String[] args) {
+		for (int i = 9398; i <= 10178; i++) {
+			System.out.println ((char) i);
 		}
-		for ( int i = 33; i <= 126; i++ ) {
-			System.out.println ( (char) i );
+
+		for (int i = 33; i <= 126; i++) {
+			System.out.println ((char) i);
 		}
 	}
-	
-	
-	
 }
