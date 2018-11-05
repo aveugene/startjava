60bc538e96546b4c934a4f7487a148062996eb57
aveugene <eugeneavilov@gmail.com>
2018-11-06 06:01:30 +0700

    lesson4. calculator.

--- src/com/startjava/lesson_1/calculator/Calculator.java
+++ src/com/startjava/lesson_1/calculator/Calculator.java
@@ -23,7 +23,7 @@ class Calculator {
 		}
 	}
 	
-	public static int raiseToPower(int base, int exponent) {
+	private static int raiseToPower(int base, int exponent) {
 		int result = 1;
 		for (int i = 0; i < exponent; i++) {
 			result *= base;
--- src/com/startjava/lesson_2_3/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3/calculator/Calculator.java
@@ -12,27 +12,27 @@ class Calculator {
 	public void setSecondNumber(int secondNumber) {
 		this.secondNumber = secondNumber;
 	}
-	
+
 	public void setSign(char sign) {
 		this.sign = sign;
 	}
 
 	public void calculate() {
-		switch (sign){
+		switch (sign) {
 			case '+':
-				System.out.println(firstNumber + "" + sign + "" + secondNumber + "=" + (firstNumber + secondNumber));
+				System.out.println(firstNumber + "" + sign + "" + secondNumber + "=" + Math.addExact(firstNumber, secondNumber));
 				break;
 			case '-':
-				System.out.println(firstNumber + "" + sign + "" + secondNumber + "=" + (firstNumber - secondNumber));
+				System.out.println(firstNumber + "" + sign + "" + secondNumber + "=" + Math.subtractExact(firstNumber, secondNumber));
 				break;
 			case '*':
-				System.out.println(firstNumber + "" + sign + "" + secondNumber + "=" + (firstNumber * secondNumber));
+				System.out.println(firstNumber + "" + sign + "" + secondNumber + "=" + Math.multiplyExact(firstNumber, secondNumber));
 				break;
 			case '/':
-				System.out.println(firstNumber + "" + sign + "" + secondNumber + "=" + (firstNumber / secondNumber));
+				System.out.println(firstNumber + "" + sign + "" + secondNumber + "=" + Math.floorDiv(firstNumber, secondNumber));
 				break;
 			case '^':
-				System.out.println(firstNumber + "" + sign + "" + secondNumber + "=" + raiseToPower(firstNumber, secondNumber));
+				System.out.println(firstNumber + "" + sign + "" + secondNumber + "=" + (int) Math.pow(firstNumber, secondNumber));
 				break;
 			case '%':
 				System.out.println(firstNumber + "" + sign + "" + secondNumber + "=" + (firstNumber % secondNumber));
@@ -41,14 +41,4 @@ class Calculator {
 				System.out.println("The operation " + sign + " is invalid.");
 		}
 	}
-	
-	private int raiseToPower(int base, int exponent) {
-		int result = 1;
-		for (int i = 0; i < exponent; i++) {
-			result *= base;
-		}
-		return result;
-	}
-
-
 }
